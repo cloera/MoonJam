0de9626cbe5c2710302ca7d0264d852528ef8d52
Dylan Hicks <dman1366@gmail.com>
2021-05-29 09:19:18 -0600

    Add Temp Enemy with Object Shredder.

--- MoonJam/Assets/Scripts/Enemy.cs
+++ MoonJam/Assets/Scripts/Enemy.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Enemy : MonoBehaviour {
     [Header("Enemy Configs")]
     [SerializeField] private float health = 100f;
+    [SerializeField] private float movementSpeed = 10f;
 
     [Header("Projectile Configs (Optional)")]
     [SerializeField] private GameObject projectile;
@@ -17,5 +18,14 @@ public class Enemy : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        move();
+    }
+
+    private void move() {
+        Vector3 movement = new Vector3(movementSpeed * Time.deltaTime, 0, 0);
+
+        Vector3 destination = transform.position - movement;
+
+        transform.position = destination;
     }
 }
