7f58d2c32f47f7833a69f07075c5a9ba81c74540
Dylan Hicks <dman1366@gmail.com>
2021-05-31 00:56:37 -0600

    Add sorting layers to enemies.

--- MoonJam/Assets/Scripts/Lane.cs
+++ MoonJam/Assets/Scripts/Lane.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Lane : MonoBehaviour {
     [SerializeField] private GameObject spawner = null;
+
+    // Cache
+    private SortingLayer sortingLayer;
 
     // Start is called before the first frame update
     void Start() {
@@ -15,11 +19,19 @@ public class Lane : MonoBehaviour {
 
     public void spawnEnemy(GameObject enemyToSpawn) {
         if (enemyToSpawn != null) {
-            Instantiate(
+            GameObject enemyClone = Instantiate(
                 enemyToSpawn,
                 spawner.transform.position,
                 Quaternion.identity
             );
+
+            Enemy enemy = enemyClone.GetComponent<Enemy>();
+
+            enemy.setSortingGroup(getSortingGroup());
         }
+    }
+
+    private SortingGroup getSortingGroup() {
+        return gameObject.GetComponent<SortingGroup>();
     }
 }
